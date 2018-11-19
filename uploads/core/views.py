a7372fd98a71ce45bc742df8bc57c21e252b4aed
anvitgupta <anvit.gupta@vanderbilt.edu>
2018-11-19 03:00:15 -0600

    bootstrap add

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -5,11 +5,9 @@ from django.core.files.storage import FileSystemStorage
 from uploads.core.models import Document
 from uploads.core.forms import DocumentForm
 
-
 def home(request):
     documents = Document.objects.all()
     return render(request, 'core/home.html', { 'documents': documents })
-
 
 def simple_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
@@ -21,7 +19,6 @@ def simple_upload(request):
             'uploaded_file_url': uploaded_file_url
         })
     return render(request, 'core/simple_upload.html')
-
 
 def model_form_upload(request):
     if request.method == 'POST':
