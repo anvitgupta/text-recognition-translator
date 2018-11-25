aca036d7960d634d81d835671bb43987939e5da9
Max Engel <maxengel99@gmail.com>
2018-11-24 20:55:33 -0600

    add image handler

---
+++ Translation/imageprocessinghandler.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import pytesseract
+
+text = pytesseract.image_to_string(Image.open('test.png'))
+
+print (text)
+
+
+class ImageProcessingHandler:
+
+    def GetTextFromImage(self, image):
+        return pytesseract.image_to_string(image)
