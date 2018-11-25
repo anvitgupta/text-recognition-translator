c9a27f1694b4ca0c7cb9f44ce00b04d02f8f3d5c
Max Engel <maxengel99@gmail.com>
2018-11-24 22:16:01 -0600

    fix sound file path

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -27,7 +27,7 @@ def simple_upload(request):
         sound_file = soundconverter.ConvertToSound(translated_text)
         path = './media/sound.mp3'
         sound_file.save(path)
-        sound_file_url = fs.url(path)
+        sound_file_url = fs.url("sound.mp3")
 
         return render(request, 'core/simple_upload.html', {
             'uploaded_file_url': uploaded_file_url,
