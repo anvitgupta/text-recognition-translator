0a2c783ecfef626f10f6c6a613e566582f1f55d8
anvitgupta <anvit.gupta@vanderbilt.edu>
2018-12-03 21:59:31 -0600

    adding tests

--- uploads/core/tests.py
+++ uploads/core/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from views import home, simple_upload, get_file, process_text, save_sound_file
+from django.http import HttpRequest
 
-# Create your tests here.
+request = HttpRequest()
+request.method = 'POST'
+request.META['language'] = 'spa'
+requst.FILES[''] = 
