eb8fc8b7c0bace9941b5521195b02d189de70a44
Max Engel <maxengel99@gmail.com>
2018-12-07 19:45:45 -0600

    update text extraction test

--- Translation/imageprocessinghandler.py
+++ Translation/imageprocessinghandler.py
@@ -1,11 +1,6 @@
 from PIL import Image
 import pytesseract
 
-# text = pytesseract.image_to_string(Image.open('test.png'))
-
-# print (text)
-
-
 class ImageProcessingHandler:
 
     def GetTextFromImage(self, image, language):
