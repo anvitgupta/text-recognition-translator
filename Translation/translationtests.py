d5252289ad7e6fe7d4e038e7953d86a398430754
Max Engel <maxengel99@gmail.com>
2018-12-07 19:48:54 -0600

    update unit tests

--- Translation/translationtests.py
+++ Translation/translationtests.py
@@ -4,17 +4,25 @@ from imageprocessinghandler import ImageProcessingHandler
 
 class TranslationTests(unittest.TestCase):
 
+    def testgettextfromimage(self):
+        imageprocessor = ImageProcessingHandler()
+        text = imageprocessor.GetTextFromImage("spanish.png", "spa")
+        self.assertEqual(text, "Yo vivo en Nashville")
+
     def testdetectenglish(self):    
         translator = TranslationHandler()
-        self.assertEqual(translator.DetectLanguage("this is english").lang, 'en')
+        language = translator.DetectLanguage("this is english").lang
+        self.assertEqual(language, 'en')
     
     def testdetectspanish(self):
         translator = TranslationHandler()
-        self.assertEqual(translator.DetectLanguage('esto es espanol').lang, 'es')
+        language = translator.DetectLanguage('おはようございます').lang
+        self.assertEqual(language, 'ja')
 
     def testtranslatetext(self):
         translator = TranslationHandler()
-        self.assertEqual(translator.TranslateText('esto es espanol').text, 'this is Spanish')
+        text = translator.TranslateText('esto es espanol').text
+        self.assertEqual(text, 'this is Spanish')
 
 if __name__=='__main__':
     unittest.main()
